86f900675ece5e0669f684953d3e4ac90a0fcf06
devjjongs <whdtjr5689@gmail.com>
2021-08-24 15:59:23 +0900

    clone repository

---
+++ src/main/java/com/bit203/vinoweknow/VinoweknowApplication.java
@@ -0,0 +1,13 @@
+package com.bit203.vinoweknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinoweknowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinoweknowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bit203/vinoweknow/VinoweknowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit203.vinoweknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinoweknowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
