132d4b27992ecfc931138a7db03c07882673e151
devjjongs <whdtjr5689@gmail.com>
2021-08-24 16:09:51 +0900

    added file -Member, MemberDao

--- src/main/java/com/bit203/vinoweknow/model/vo/Member.java
+++ src/main/java/com/bit203/vinoweknow/model/vo/Member.java
@@ -1,4 +1,14 @@
 package com.bit203.vinoweknow.model.vo;
 
+import lombok.Data;
+
+@Data
 public class Member {
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String email;
+    private int age;
+    private boolean isActive;
+    private boolean isAdmin;
 }
