b190fc6596c728d077a79ef3596981ef483f9de0
hayoonpark <gkdbs0120@gmail.com>
2021-08-30 16:29:27 +0900

    create MVC pattern

---
+++ src/main/java/com/bit203/vinoweknow/vo/Subscription.java
@@ -0,0 +1,13 @@
+package com.bit203.vinoweknow.vo;
+
+public class Subscription {
+    private String subCode;
+    private String memCode;
+    private String subAddr;
+    private String subType;
+    private String subPayPlan;
+    private int subQuantity;
+    private String subStartDate;
+    private String subEndDate;
+    private String subProductCode;
+}
