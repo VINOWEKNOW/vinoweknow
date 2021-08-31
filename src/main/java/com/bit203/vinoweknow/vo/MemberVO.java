d945ab74926db7323a5591f2e9d7fa4c7d6bd722
devjjongs <whdtjr5689@gmail.com>
2021-08-31 16:22:54 +0900

    modified files

--- src/main/java/com/bit203/vinoweknow/vo/MemberVO.java
+++ src/main/java/com/bit203/vinoweknow/vo/MemberVO.java
@@ -2,11 +2,15 @@ package com.bit203.vinoweknow.vo;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class MemberVO {
-    private String userId;
-    private String userPw;
-    private String userName;
+    private int memberCode;
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+    private LocalDate memberBirth;
     private String email;
     private int age;
     private boolean isAdmin;
