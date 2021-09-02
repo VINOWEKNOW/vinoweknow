5922333b02d6690c195703ce3e5d9c0a044fed2b
YOGA-SLIM7\hostf <hostface@naver.com>
2021-09-02 12:01:28 +0900

    VO 통합

--- src/main/java/com/bit203/vinoweknow/vo/MemberVO.java
+++ src/main/java/com/bit203/vinoweknow/vo/MemberVO.java
@@ -2,23 +2,6 @@ package com.bit203.vinoweknow.vo;
 
 import lombok.Data;
 
-<<<<<<< HEAD
-import java.util.Date;
-
-@Data
-public class MemberVO {
-    private int userCode;
-    private String userId;
-    private String userPw;
-    private String userName;
-    private char userGender;
-    private Date userBirth;
-    private String userAddr;
-    private String userTel;
-    private String email;
-    private Date userSuDate;
-    private int age;
-=======
 import java.time.LocalDate;
 
 @Data
@@ -34,6 +17,5 @@ public class MemberVO {
     private String memberEmail;
     private LocalDate memberSignUp;
     private int subscriptStatus;
->>>>>>> c8330ac82826b876661089507f85628b3d159938
     private boolean isAdmin;
 }
