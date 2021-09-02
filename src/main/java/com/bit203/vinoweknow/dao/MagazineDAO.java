e2b6e4c05384f26923744c17510042ae6d9fd671
YOGA-SLIM7\hostf <hostface@naver.com>
2021-09-02 12:57:57 +0900

    메거진다오에서 멤버다오로 메서드 옮김

--- src/main/java/com/bit203/vinoweknow/dao/MagazineDAO.java
+++ src/main/java/com/bit203/vinoweknow/dao/MagazineDAO.java
@@ -5,10 +5,5 @@ import com.bit203.vinoweknow.vo.MemberVO;
 import java.util.ArrayList;
 
 public interface MagazineDAO {
-    void insertMem();
-    void deleteMem();
-    ArrayList<MemberVO> memList();
-    MemberVO searchMem();
-    void login();
-    void logout();
+
 }
--- src/main/java/com/bit203/vinoweknow/dao/MemberDao.java
+++ src/main/java/com/bit203/vinoweknow/dao/MemberDao.java
@@ -1,4 +1,14 @@
 package com.bit203.vinoweknow.dao;
 
+import com.bit203.vinoweknow.vo.MemberVO;
+
+import java.util.ArrayList;
+
 public interface MemberDao {
+    void insertMem();
+    void deleteMem();
+    ArrayList<MemberVO> memList();
+    MemberVO searchMem();
+    void login();
+    void logout();
 }
