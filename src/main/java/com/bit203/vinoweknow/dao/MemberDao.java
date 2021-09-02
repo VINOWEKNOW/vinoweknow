c3e61cdf4a3f1136ae266921f271912a934cd88a
devjjongs <whdtjr5689@gmail.com>
2021-09-02 12:11:58 +0900

    added & modified files -added joinForm.html, MemberMapper.xml, MemberPreferenceVO class -modified WineVO, MemberDao, application.yml

--- src/main/java/com/bit203/vinoweknow/dao/MemberDao.java
+++ src/main/java/com/bit203/vinoweknow/dao/MemberDao.java
@@ -1,4 +1,10 @@
 package com.bit203.vinoweknow.dao;
 
+import com.bit203.vinoweknow.vo.MagazineVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MemberDao {
+
+    void insertMember(MagazineVO member);
 }
