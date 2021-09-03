5e4c13de39504e5331a7b163ca8a98e76937f1a3
devjjongs <whdtjr5689@gmail.com>
2021-09-03 10:23:34 +0900

    added files -about oracle jdbc modified files -modified joinForm.html

--- src/main/java/com/bit203/vinoweknow/dao/MemberDao.java
+++ src/main/java/com/bit203/vinoweknow/dao/MemberDao.java
@@ -1,10 +1,20 @@
 package com.bit203.vinoweknow.dao;
 
-import com.bit203.vinoweknow.vo.MagazineVO;
+import com.bit203.vinoweknow.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemberDao {
 
-    void insertMember(MagazineVO member);
+    void insertMember(MemberVO member);
+
+    void selectMember(MemberVO member);
+
+    List<MemberVO> memberList();
+
+    void updateMember(MemberVO member);
+
+    void deleteMember(MemberVO member);
 }
