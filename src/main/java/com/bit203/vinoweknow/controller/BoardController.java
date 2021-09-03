d17168e1937b6aeff80dfb9dc87d2b54d7d33948
Jangheejang <wkdgml5714@naver.com>
2021-09-03 22:37:59 +0900

    Board VO,DAO add

--- src/main/java/com/bit203/vinoweknow/controller/BoardController.java
+++ src/main/java/com/bit203/vinoweknow/controller/BoardController.java
@@ -1,4 +1,11 @@
 package com.bit203.vinoweknow.controller;
 
+import com.bit203.vinoweknow.dao.BoardDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BoardController {
+
+    @Autowired
+    private BoardDAO boardDAO;
+
 }
