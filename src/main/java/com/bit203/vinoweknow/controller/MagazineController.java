ba78eb78e2bbc7cce5d0a5e22040e5430589943b
YOGA-SLIM7\hostf <hostface@naver.com>
2021-09-09 09:50:22 +0900

    MagazineDAO 메서드 작성 MyPage 생성

--- src/main/java/com/bit203/vinoweknow/controller/MagazineController.java
+++ src/main/java/com/bit203/vinoweknow/controller/MagazineController.java
@@ -1,5 +1,11 @@
 package com.bit203.vinoweknow.controller;
 
+import com.bit203.vinoweknow.dao.MagazineDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MagazineController {
+
+    @Autowired
+    private MagazineDAO magazineDAO;
 
 }
